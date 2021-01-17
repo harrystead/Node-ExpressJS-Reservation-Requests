775e5eb3258c1d6261bbffc555873ee1eef304dc
Harry Stead <harrystead9@gmail.com>
2021-01-17 13:36:34 +0000

    changed home path

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const waitingListData = [
 
 //----------------------------------------------------\\
 
-app.get("/home", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
